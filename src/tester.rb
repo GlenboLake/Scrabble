dd8bb35935e84b2fb2d16a97e4cb675b45b4c2b7
Glen Haber <glenbolake@gmail.com>
2011-10-31 16:35:42 -0400

    Modified test script TODO: modify Board::score_turn to include full words and cross-sections

--- src/tester.rb
+++ src/tester.rb
@@ -4,15 +4,18 @@
 $LOAD_PATH << '.'
 
 require 'board'
+require 'player'
 
-print ?A.class.name
+players = Array.new
+players[0] = Player.new('Glen')
+players[1] = Player.new('Fool')
+
+current_player = 0
 
 board = Board.new
-board.print
 
+# First turn: RABBLE
 turn = Turn.new
-turn.add_letter(?S, 7, 5)
-turn.add_letter(?C, 7, 6)
 turn.add_letter(?R, 7, 7) # DWS
 turn.add_letter(?A, 7, 8)
 turn.add_letter(?Q, 1, 1)
@@ -22,15 +25,58 @@ turn.add_letter(?L, 7, 11) #DLS
 turn.add_letter(?E, 7, 12)
 turn.remove_letter(1, 1)
 
-puts(board.play_turn(turn))
+# Play the turn
+turn_score = board.play_turn(turn)
+if (turn_score > 0)
+  puts("#{players[current_player].name} scored #{turn_score} points!")
+  players[current_player].turns << turn
+  players[current_player].score += turn_score
+end
 board.print
+# Print the scores
+current_player = (current_player+1) % players.size
+players.each do |player|
+  puts("#{player.name}: #{player.score}")
+end
 
+# Second turn: SCRABBLE
+turn = Turn.new
+turn.add_letter(?S, 7, 5) # DWS
+turn.add_letter(?C, 7, 6)
+
+# Play the turn
+turn_score = board.play_turn(turn)
+if (turn_score > 0)
+  puts("#{players[current_player].name} scored #{turn_score} points!")
+  players[current_player].turns << turn
+  players[current_player].score += turn_score
+end
+board.print
+#Print the scores
+current_player = (current_player+1) % players.size
+players.each do |player|
+  puts("#{player.name}: #{player.score}")
+end
+
+# Third turn: ROCKS
 turn = Turn.new
 turn.add_letter(?R, 5, 6)
 turn.add_letter(?O, 6, 6)
 turn.add_letter(?K, 8, 6)
 turn.add_letter(?S, 9, 6)
 
-puts(board.play_turn(turn))
+# Play the turn
+turn_score = board.play_turn(turn)
+if (turn_score > 0)
+  puts("#{players[current_player].name} scored #{turn_score} points!")
+  players[current_player].turns << turn
+  players[current_player].score += turn_score
+end
 board.print
+#Print the scores
+current_player = (current_player+1) % players.size
+players.each do |player|
+  puts("#{player.name}: #{player.score}")
+end
 
+puts("Expected 36 to 15")
